4579ca5efc6b548589bb70a97fd92bfd24889323
Tissyane <tissyane@gmail.com>
2022-09-09 17:32:44 -0300

    Post request - SignUp/Create User

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,22 +1,32 @@
-import bcrypt from "bcrypt";
 import db from "../database/db.js";
 import { userSchema } from "../utils/Schema.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 async function createUser(req, res) {
   const user = req.body;
 
-  const validation = userSchema.validate({ user });
+  // const validation = userSchema.validate({ user });
 
-  if (validation.error) {
-    const loginError = validation.error.details[0].message;
-    return res.status(422).send(loginError);
+  // if (validation.error) {
+  //   const loginError = validation.error.details[0].message;
+  //   return res.status(422).send(loginError);
+  // }
+
+  const passwordHash = bcrypt.hashSync(user.password.toString(), 10);
+
+  try {
+    const isUser = await db.collection("users").findOne({ email: user.email });
+
+    if (isUser) {
+      return res.status(409).send("Usuário já cadastrado");
+    }
+    await db.collection("users").insertOne({ ...user, password: passwordHash });
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err);
   }
-
-  const passwordHash = bcrypt.hashSync(user.password, 10);
-
-  await db.collection("users").insertOne({ ...user, password: passwordHash });
-
-  res.sendStatus(201);
 }
 
 export { createUser };
